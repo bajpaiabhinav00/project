6caa245a81737596adcf56ff5c45d2533e862f63
Abhinav Kumar Bajpai <bajpaiabhinav00@gmail.com>
2024-11-14 20:53:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ function scrollToSection(sectionId) {
     event.preventDefault();
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
     const message = document.getElementById('message').value.trim();
     const response = document.getElementById('form-response');
   
-    if (name && email && message) {
+    if (name && email && phone && message) {
       response.textContent = `Thank you, ${name}. We will respond soon!`;
       response.style.color = 'green';
       document.getElementById('contact-form').reset();
